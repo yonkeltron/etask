362a370bf20cdbf4dec93a21b3f3ef20bb8ca117
yonkeltron <yonkeltron@gmail.com>
2009-04-14 01:59:36 -0400

    initial commit

---
+++ etasks.el
@@ -0,0 +1,66 @@
+;;;; ETasks System for Rake-like task management for Emacs
+;;;; Jonathan E. Magen
+
+;;; Set things up
+(defvar *etask-tasks* (make-hash-table))
+
+;;; internally-used-utility-functions
+(defun ok-to-mess-with-file-p (source &optional dest)
+  (and (file-exists-p source) 
+       (file-readable-p source)
+       (file-writable-p dest)))
+
+;;; exec a command with the shell
+(defun sh (command-string)
+  "Executes a command string"
+  (shell-command-to-string (shell-quote-argument command-string)))
+
+
+;;; file manipulation helpers
+(defun cp (source dest)
+  "Copies a file from source to dest"
+  (if (ok-to-mess-with-file-p source dest)
+      (copy-file source dest)))
+
+(defun mv (source dest)
+  "Moves (renames) a file from source to dest"
+  (if (ok-to-mess-with-file-p source dest)
+      (rename-file source dest)))
+
+(defun rm (filename)
+  "Removes (deletes) a file called filename"
+  (if (ok-to-mess-with-file-p source dest)
+      (delete-file filename)))
+
+;;; filelists and filelist iteration
+(defun filelist (pattern)
+  "Returns a list with all files matched by the expression pattern"
+  (file-expand-wildcards pattern))
+
+(defun filelist-each (pattern action)
+  "Perform an action on each file matched by pattern"
+  (let ((files (filelist pattern)))
+    (dolist (file files)
+      (action file))))
+    
+;;; main task code
+(defun task (name commands &optional deps)
+  "Creates a new task"
+;;  (interactive)
+  (let ((task-object (make-hash-table)))
+    ;construct task-object
+    (puthash 'task-name name task-object)
+    (puthash 'action commands task-object)
+    (puthash 'deps deps task-object)
+    ;put task-object into global task list
+    (puthash name task-object *etask-tasks*)))
+
+(defun etask-run-task (taskname)
+  "Runs a task with the given name"
+  (interactive)
+
+  (let ((task (gethash taskname *etask-tasks* nil)))
+    (if (task)
+	(progn
+	  (message (concat "Beginning run of task " (gethash 'name task nil) " at " (current-time-string)))
+	  ((gethash 'action task nil))))))
